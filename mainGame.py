bfeb96ec6422899ad22dab5fe165e1c61f084d50
jackHorwell <65086541+jackHorwell@users.noreply.github.com>
2021-09-17 18:58:20 +0100

    Create mainGame.py
    Added first iteration of the main game

---
+++ mainGame.py
@@ -0,0 +1,208 @@
+# Imports modules to control raspberry pi GPIO
+import RPi.GPIO as GPIO
+import time
+
+# Sets up Stockfish engine
+from stockfish import Stockfish
+stockfish = Stockfish("/home/pi/Downloads/a-stockf")
+stockfish = Stockfish(parameters={"Threads": 2, "Minimum Thinking Time": 30})
+
+# Class to store piece attributes like colour and type
+class Piece:
+    def __init__(self, colour, pieceType, icon):
+        self.colour = "white"
+        self.pieceType = "pieceType"
+        self.icon = icon
+
+# Class to group together piece initialisation
+class Pieces:
+    def __init__(self):scan
+        # Initalises the piece objects and passes their attributes
+        self.wp = Piece("white", "pawn", "♙")
+        self.wr = Piece("white", "rook", "♖")
+        self.wkn = Piece("white", "knight", "♞")
+        self.wb = Piece("white", "bishop", "♗")
+        self.wq = Piece("white", "queen", "♕")
+        self.wk = Piece("white", "king", "♔")
+
+        self.bp = Piece("black", "pawn", "♟")
+        self.br = Piece("black", "rook", "♜")
+        self.bkn = Piece("black", "knight", "♞")
+        self.bb = Piece("black", "bishop", "♝")
+        self.bq = Piece("black", "queen", "♛")
+        self.bk = Piece("black", "king", "♚")
+
+# Class to store the current state and last known legal state of the board
+class BoardRecord:
+
+    def __init__(self):
+        
+        # Stack to store previous move
+        self.previousMoves = []
+        
+        # Creates a 2D array to store piece locations
+        p = Pieces()
+        
+        self.board = [[p.wr,p.wkn,p.wb,p.wq,p.wk,p.wb,p.wkn,p.wr],
+                 [p.wp,p.wp,p.wp,p.wp,p.wp,p.wp,p.wp,p.wp],
+                 [0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0],
+                 [p.bp,p.bp,p.bp,p.bp,p.bp,p.bp,p.bp,p.bp],
+                 [p.br,p.bkn,p.bb,p.bq,p.bk,p.bb,p.bkn,p.br]]
+
+    # Getter for board variable
+    def getBoard(self):
+        # Prints each row and converts objects into their assigned icons
+        for row in range(7, -1, -1):
+            rowToPrint = []
+            for square in self.board[row]:
+                if square != 0:   
+                    rowToPrint.append(square.icon)
+                else:
+                    rowToPrint.append("0 ")
+            print("".join(rowToPrint))
+
+        return self.board
+
+
+    # Setter for board variable
+    # Takes two coordinates and moves a piece to it's new position
+    def setBoard(self, pieceFrom, pieceTo, move):
+        self.board[pieceTo[1]][pieceTo[0]] = self.board[pieceFrom[1]][pieceFrom[0]]
+        
+        self.board[pieceFrom[1]][pieceFrom[0]] = 0
+
+        # Adds move made to the previousMoves array
+        self.previousMoves.append(move)
+
+        # Updates the moves made for the engine
+        stockfish.set_position(self.previousMoves)
+
+
+# Stores the pin numbers used for the rows and columns
+# Index + 1 is the row number
+rowPins = [23, 18, 11, 9, 10, 22, 27, 17]
+columnPins = [21, 20, 16, 12, 7, 8, 25, 24]
+
+# Sets the pins to be inputs or outputs
+def pinSetup():
+    GPIO.setmode(GPIO.BCM)
+    
+    for pin in rowPins:
+        GPIO.setup(pin, GPIO.OUT)
+
+    for pin in columnPins:
+        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# Function to turn off all the row pins to make sure none are accidently left on
+def resetRowPins():
+    for pin in rowPins:
+        GPIO.output(pin, 0)
+
+# Function to return the position of pieces in a 2D array
+def scanCurrentBoard():
+    board = []
+    
+    # Provides power to rows one by one and checks columns for input
+    # Stores the output to board array
+    for row in range(8):
+        board.append([])
+        GPIO.output(rowPins[row], 1)
+        for column in range(8):
+            board[row].append(GPIO.input(columnPins[column]))
+
+        GPIO.output(rowPins[row], 0)
+
+    return board
+    
+    '''
+    Used for development purposes, delete in final
+    '''
+    for i in range(7, -1, -1):
+        print(" ".join(map(str,board[i])))
+
+    print("\n\n")
+
+# Takes the current state of the board and repeatedly checks
+# for a change. Once found will return the position of the change
+def reportChange():
+    oldBoard = scanCurrentBoard()
+    
+    while True:
+        newBoard = scanCurrentBoard()
+        
+        if newBoard != oldBoard:
+            for row in range(8):
+                if newBoard[row] != oldBoard[row]:
+                    for column in range(8):
+                        if newBoard[row][column] != oldBoard[row][column]:
+                            resetRowPins()
+                            return [column, row]
+
+# Function to make sure that the pieces are where they're meant to be
+
+
+# Function to tell player where to move pieces
+def moveComputerPieces(moveFrom, moveTo, move):
+    # Tells user which piece to pick up, checks for piece removal
+    GPIO.output(rowPins[moveFrom[0]], 1)
+    print(f"Move piece from {move[0:2]}")
+    while GPIO.input(columnPins[moveFrom[1]]):
+        pass
+    GPIO.output(rowPins[moveFrom[1]], 0)
+
+    # Checks if a piece is being taken, if so tells user to remove it
+    board = currentBoard.getBoard()
+    if board[moveTo[0]][moveTo[1]] != 0:
+        GPIO.output(rowPins[moveTo[1]], 1)
+        print(f"Remove piece from {move[2:]}")
+        while GPIO.input(columnPins[moveTo[0]]):
+            pass
+        GPIO.output(rowPins[moveTo[1]], 0)
+
+    # Tells user where to move piece
+    GPIO.output(rowPins[moveTo[1]], 1)
+    print(f"Move piece to {move[2:]}")
+    while not GPIO.input(columnPins[moveTo[0]]):
+        pass
+    GPIO.output(rowPins[moveTo[1]], 0)
+
+    resetRowPins()
+
+
+# Function to obtain the move a player makes
+def getPlayerMove():
+    fromMove = reportChange()
+    print(f"From: {fromMove}")
+    toMove = reportChange()
+    print(f"To {toMove}")
+
+    move = chr(ord('a') + fromMove[0]) + str(fromMove[1] + 1) + chr(ord('a') + toMove[0]) + str(toMove[1] + 1)
+
+    if stockfish.is_move_correct(move):
+        print("legal shmegle")
+        currentBoard.setBoard(fromMove, toMove, move)
+        currentBoard.getBoard()
+    else:
+        print("Not a legal move")
+        getPlayerMove()
+        return
+
+# Function to get the AI to generate a move
+def generateEnemyMove():
+    move = stockfish.get_best_move()
+    fromMove = [int(move[1]) - 1, ord(move[0]) - 97]
+    toMove = [int(move[3]) - 1, ord(move[2]) - 97]
+    currentBoard.setBoard(fromMove, toMove, move)
+
+    currentBoard.getBoard()
+    moveComputerPieces(fromMove, toMove, move)
+
+currentBoard = BoardRecord()
+legalBoard = BoardRecord()
+pinSetup()
+
+getPlayerMove()
+generateEnemyMove()
