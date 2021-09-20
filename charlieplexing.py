b895499b229ff7bfb798b85bc729fb20e2ef09b3
jackHorwell <65086541+jackHorwell@users.noreply.github.com>
2021-09-20 20:47:35 +0100

    Update charlieplexing.py
    Changed name of charlieplexOn function to turnOn
    Added functions to produce patterns with the LEDs

--- charlieplexing.py
+++ charlieplexing.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 
 # Function that takes the LED number you want to turn on and
 # turns on/off the corresponding pins
-def charlieplexOn(turnOn):
+def turnOn(led):
     # Array that stores which pins should be high or low
     # for the charlieplex to work, first number indicates
     # pin to be high and second low. Omits pins that should
@@ -40,7 +40,38 @@ def charlieplexOn(turnOn):
     GPIO.setup(26, GPIO.IN)
 
     # Resets necessary pins to be outputs and set to HIGH or LOW
-    GPIO.setup(charlieplexingPins[turnOn][0], GPIO.OUT)
-    GPIO.setup(charlieplexingPins[turnOn][1], GPIO.OUT)
-    GPIO.output(charlieplexingPins[turnOn][0], GPIO.HIGH)
-    GPIO.output(charlieplexingPins[turnOn][1], GPIO.LOW)
+    GPIO.setup(charlieplexingPins[led][0], GPIO.OUT)
+    GPIO.setup(charlieplexingPins[led][1], GPIO.OUT)
+    GPIO.output(charlieplexingPins[led][0], GPIO.HIGH)
+    GPIO.output(charlieplexingPins[led][1], GPIO.LOW)
+
+
+# Function to flash all LEDs 3 times
+def allFlash():
+    for i in range(3):
+        for j in range(1000):
+            for k in range(17):
+                turnOn(k)
+
+        turnOn(0)
+        time.sleep(0.2)
+
+
+# Function to turn on each LED one at a time in order
+# Speed depends on the input
+def slide(speed):
+    if speed == "fast":
+        delay = 0.05
+    elif speed == "slow":
+        delay = 0.5
+        
+    for i in range(1, 17):
+        turnOn(i)
+        time.sleep(delay)
+
+# Function to have LED "bounce" back and forth
+def bounce():
+    for i in range(1, 17):
+        for j in range(2000):
+            turnOn(i)
+            turnOn(17 - i)
